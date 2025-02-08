be4989f4914d4d5f1e6d74223eb6c6c196563980
sheikh-hammad <hammadusman001@gmail.com>
2025-02-08 12:36:51 +0500

    resolve issue in product detail page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,23 +1,17 @@
 import { notFound } from "next/navigation";
-import {Image} from 'sanity'
+import { Image } from "sanity";
 import { Prods } from "../../components/data/types";
-import {client} from "../../../sanity/lib/client"
+import { client } from "../../../sanity/lib/client";
 import Click from "@/app/components/ui/Click";
 import FeatureComponent from "@/app/components/ui/FeatureComponent";
 import { urlFor } from "@/sanity/lib/image";
-
-// interface Product {
-//   id: number;
-//   name: string;
-//   price: number;
-//   src: string;
-// }
 
 interface Params {
   params: {
     id: string;
   };
 }
+
 interface IProduct {
   title: string;
   desc: string;
@@ -26,26 +20,10 @@ interface IProduct {
   price: string;
   _id: string;
   priceWithoutDiscount: string;
-  tagClass?: string,
-  cartClass?: string,
-  priceClass?: string,
-  nameClass?: string,
-}
-
-const getProductData = async () => {
-  const res = await client.fetch(`*[_type=='products']{
-    title,
-    _id,
-    badge, 
-    priceWithoutDiscount,
-    category,
-    price,
-    description,
-    tags -> ,
-    image
-    
-}`)
-return res
+  tagClass?: string;
+  cartClass?: string;
+  priceClass?: string;
+  nameClass?: string;
 }
 
 export async function generateStaticParams() {
@@ -55,18 +33,34 @@ export async function generateStaticParams() {
 }
 
 export default async function ProductPage({ params }: Params) {
-    const products:IProduct[] = await getProductData();
-  
+  const getProductData = async () => {
+    return await client.fetch(`*[_type=='products']{
+      title,
+      _id,
+      badge, 
+      priceWithoutDiscount,
+      category,
+      price,
+      description,
+      tags -> ,
+      image
+    }`);
+  };
+
+  const products: IProduct[] = await getProductData();
   const product = products.find((prod) => prod._id === params.id);
-  console.log({product})
 
   if (!product) {
     notFound();
   }
 
   return (
-    <div className="pt-[57px] pb-[173px] px-4 sm:px-[25px] md:px-[120px]  2xl:px-60 3xl:px-[265px] xl:px-[150px] lg:px-[100px]">
-     <Click name={product.title} price={product.price} src={urlFor(product.image).width(500).url()} />
+    <div className="pt-[57px] pb-[173px] px-4 sm:px-[25px] md:px-[120px] 2xl:px-60 3xl:px-[265px] xl:px-[150px] lg:px-[100px]">
+      <Click
+        name={product.title}
+        price={product.price}
+        src={urlFor(product.image).width(500).url()}
+      />
       <FeatureComponent />
     </div>
   );
